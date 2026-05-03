13a6da3a1f722324e854b397d4da52dceec96398
Stefano Capurro-spec <274860826+stefanocapurro-spec@users.noreply.github.com>
2026-05-03 20:10:38 +0200

    PWA: display_override for desktop

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
         theme_color: '#0f1117',
         background_color: '#0a0c10',
         display: 'standalone',
+        display_override: ['window-controls-overlay', 'standalone'],  //
         orientation: 'portrait-primary',
         start_url: base,
         scope: base,
